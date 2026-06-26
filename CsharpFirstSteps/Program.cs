e9bdaa9f1957701f166f84ccaeeae988dbffed54
Allan <allanfelipedk@gmail.com>
2026-06-26 10:43:18 -0300

    Adiciona primeiros passos em C#

---
+++ CsharpFirstSteps/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CsharpFirstSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola gamedevpro!");
+
+            Console.Write("Teste de comando!");
+            Console.Write("Segundo Teste");
+
+            Console.ReadKey();
+        }
+    }
+}
